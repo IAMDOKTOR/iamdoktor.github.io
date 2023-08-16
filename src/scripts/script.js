9ff1fceb77a9287e99707e4de44c2d0cc9e6c17d
IAMDOKTOR <allex.semenovv@gmail.com>
2023-08-16 00:35:15 -0400

    initial commit

---
+++ src/scripts/script.js
@@ -0,0 +1,8 @@
+
+//on click
+function fade() {
+    //get the div
+    let div = document.getElementById("fade-out");
+    //trigers animation
+    div.style.animation = "fade-out 1s !important";
+}
